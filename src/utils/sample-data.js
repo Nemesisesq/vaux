571713ca25e6f733b291e882962a06a2551b4881
Trey Hakanson <treyhakanson@gmail.com>
2018-05-13 21:09:32 -0400

    🔧 Testing messages with images

--- src/utils/sample-data.js
+++ src/utils/sample-data.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import faker from "faker";
 
+/// Sample thread ids
 export const SAMPLE_THREAD_IDS = [
    faker.random.uuid(),
    faker.random.uuid(),
@@ -13,13 +14,29 @@ export const SAMPLE_THREAD_IDS = [
    faker.random.uuid()
 ];
 
+/**
+ * Generates a URL for a randomly dimensioned placeholder image
+ * @return {String} the image URL
+ */
+function placeholderImageURL() {
+   const baseURL = "http://placeholder.pics/svg";
+   const width = Math.ceil(Math.random() * 300 + 300);
+   const height = Math.ceil(Math.random() * 300 + 100);
+   return `${baseURL}/${width}x${height}`;
+}
+
+/**
+ * Generates sample messages for test purposes.
+ * @param  {Number} [n=10] the number of sample messages to generate
+ * @return {[Object]}      an array of message objects
+ */
 export function generateSampleMessages(n = 10) {
    let messages = [];
    const userId = faker.random.uuid();
    const userName = faker.name.findName();
    const userAvatar = faker.image.avatar();
    for (let i = 0; i < n; i++) {
-      messages.push({
+      let msg = {
          _id: faker.random.uuid(),
          text: faker.lorem.sentence(),
          createdAt: moment(faker.date.past()).toDate(),
@@ -28,11 +45,22 @@ export function generateSampleMessages(n = 10) {
             name: userName,
             avatar: userAvatar
          }
-      });
+      };
+      // add an image some of the time
+      if (faker.random.boolean() && faker.random.boolean()) {
+         msg.image = placeholderImageURL();
+      }
+      messages.push(msg);
    }
    return messages;
 }
 
+/**
+ * Generates sample threads to represent users.
+ * @param  {Number} [n=10]     the number of sample threads to generate
+ * @param  {Object} [options={ threadIds:    [] }] the thread ids to use
+ * @return {[Object]}            an arrat of thread objects
+ */
 export function generateSampleThreads(n = 10, options = { threadIds: [] }) {
    let threads = [];
    for (let i = 0; i < n; i++) {
