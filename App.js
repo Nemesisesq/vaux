05b693bef6b9c51c5942d1e65979ca1d52b1a923
nemesisesq <nemesisesq@gmail.com>
2018-06-05 16:37:07 -0400

    added authentication

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { YellowBox, StatusBar, View, Text } from "react-native";
-import { AppLoading, Asset } from "expo";
-import { dispatch, Provider } from "react-redux";
+import React, {Component} from "react";
+import {YellowBox, StatusBar, View, Text} from "react-native";
+import {AppLoading, Asset} from "expo";
+import {dispatch, Provider} from "react-redux";
 
-import { store, message, networking } from "./src/ducks";
+import {store, message, networking} from "./src/ducks/index";
 import localStore from "./src/utils/local-store";
-import { ASSET_DIR, SOUNDS, BASE_URL } from "./src/constants";
-import Base from "./src";
+import {ASSET_DIR, SOUNDS, BASE_URL} from "./src/constants/index";
+import Base from "./src/index";
 import ErrorScreen from "./src/components/error-screen";
 
 /*
@@ -14,76 +14,123 @@ import ErrorScreen from "./src/components/error-screen";
  * https://github.com/facebook/react-native/issues/18175
  */
 YellowBox.ignoreWarnings([
-   "Warning: componentWillMount is deprecated",
-   "Warning: componentWillUpdate is deprecated",
-   "Warning: componentWillReceiveProps is deprecated"
+    "Warning: componentWillMount is deprecated",
+    "Warning: componentWillUpdate is deprecated",
+    "Warning: componentWillReceiveProps is deprecated"
 ]);
 
-export default class App extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         isReady: false,
-         error: null
-      };
-   }
+import Amplify, {Auth} from 'aws-amplify';
+import {Authenticator} from 'aws-amplify-react-native';
+import awsmobile from './src/aws-exports';
+import {hostUri} from "./src/config";
 
-   // TODO: pre-cache sound assets, fonts, logos, etc.
-   _performAsyncSetup = async () => {
-      // initialize local store and update redux with played sounds
-      const initRes = await localStore.initialize();
-      const setRes = await localStore.retrieveSet(
-         localStore.STORE_KEYS.PLAYED_SOUNDS
-      );
+Amplify.configure(awsmobile);
 
-      if (!initRes.success || !setRes.success) {
-         this.setState({ error: "An error occurred 😭" });
-         return;
-      }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isReady: false,
+            error: null
+        };
+    }
 
-      store.dispatch(message.setPlayedSounds(setRes.data));
-      await store.dispatch(networking.connect(BASE_URL));
-      const { error } = store.getState().networking;
-      if (error) {
-         this.setState({ error });
-         return;
-      }
+    _getUser = async () => {
+        let user = await Auth.currentAuthenticatedUser();
 
-      // pre-cache sounds
-      const soundAssets = Object.values(SOUNDS).map(sound => {
-         return Asset.fromModule(sound.module).downloadAsync();
-      });
-      await Promise.all(soundAssets);
-   };
+        let data = {
+            ...user.signInUserSession.idToken.payload,
+            jwtToken: user.signInUserSession.idToken.jwtToken
+        };
 
-   _onError = err => {
-      console.log(err);
-      this.setState({ isReady: true });
-   };
 
-   render() {
-      // TODO: style loading screen
-      if (!this.state.isReady) {
-         return (
-            <AppLoading
-               startAsync={this._performAsyncSetup}
-               onFinish={() => this.setState({ isReady: true })}
-               onError={this._onError}
-            />
-         );
-      }
+        user = {name: "", email: "", profile: ""};
 
-      if (this.state.error) {
-         return <ErrorScreen text={this.state.error} />;
-      }
 
-      return (
-         <View style={{ flex: 1 }}>
-            <StatusBar backgroundColor="transparent" barStyle="light-content" />
-            <Provider store={store}>
-               <Base />
-            </Provider>
-         </View>
-      );
-   }
+        await axios({
+            url: `http://${hostUri}/user`,
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            data: data
+        })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        return user;
+    };
+
+    // TODO: pre-cache sound assets, fonts, logos, etc.
+    _performAsyncSetup = async () => {
+        // initialize local store and update redux with played sounds
+        const initRes = await localStore.initialize();
+        const setRes = await localStore.retrieveSet(
+            localStore.STORE_KEYS.PLAYED_SOUNDS
+        );
+
+        if (!initRes.success || !setRes.success) {
+            this.setState({error: "An error occurred 😭"});
+            return;
+        }
+
+        store.dispatch(message.setPlayedSounds(setRes.data));
+
+        debugger;
+        await store.dispatch(networking.connect(`ws://${BASE_URL}/connect`));
+        const {error} = store.getState().networking;
+        if (error) {
+            this.setState({error});
+            return;
+        }
+
+        // pre-cache sounds
+        const soundAssets = Object.values(SOUNDS).map(sound => {
+            return Asset.fromModule(sound.module).downloadAsync();
+        });
+        await Promise.all(soundAssets);
+    };
+
+    _onError = err => {
+        console.log(err);
+        this.setState({isReady: true});
+    };
+
+    render() {
+        // TODO: style loading screen
+        if (!this.state.isReady) {
+            return (
+                <AppLoading
+                    startAsync={this._performAsyncSetup}
+                    onFinish={() => this.setState({isReady: true})}
+                    onError={this._onError}
+                />
+            );
+        }
+
+        if (this.state.error) {
+            return <ErrorScreen text={this.state.error}/>;
+        }
+
+        debugger
+
+        return (
+            <Authenticator>
+                <View style={{flex: 1}}>
+                    <StatusBar backgroundColor="transparent" barStyle="light-content"/>
+                    <Provider store={store}>
+                        <Base/>
+                    </Provider>
+                </View>
+            </Authenticator>
+        );
+    }
 }
+
+
+export default withAuthenticator(App);
